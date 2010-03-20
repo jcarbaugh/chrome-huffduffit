487d22dbb00f7285f3fe7161392289cb12f7de76
Jeremy Carbaugh <jcarbaugh@gmail.com>
2010-03-20 18:42:07 -0400

    add support for html5 audio and source eelements

--- contentscript.js
+++ contentscript.js
@@ -15,37 +15,39 @@ var scrapeATags = function(media) {
     }
 };
 
-// var scrapeAudioTags = function(media) {
-//     var elems = document.getElementsByTagName('audio');
-//     for (var i = 0; i < elems.length; i++) {
-//         var elem = elems[i];
-//         var file = {
-//             title: (elem.title) ? elem.title : null
-//         };
-//         if (elem.src) {
-//             if (regex.test(elem.href)) {
-//                 file.url = elem.src;
-//             }
-//         } else {
-//             var sources = elem.getElementsByTagName('source');
-//             for (var i = 0; i < sources.length; i++) {
-//                 var source = sources[i];
-//                 if (regex.test(source.href)) {
-//                     file.url = source.href;
-//                     break;
-//                 }
-//             }
-//         }
-//         if (file.url) {
-//             media.push(file);
-//         }
-//     }
-// };
+var scrapeAudioTags = function(media) {
+    var elems = document.getElementsByTagName('audio');
+    for (var i = 0; i < elems.length; i++) {
+        var elem = elems[i];
+        if (elem.src) {
+            if (regex.test(elem.src)) {
+                var file = {
+                    title: (elem.title) ? elem.title : null,
+                    url: elem.src,
+                    content: elem.title || null
+                };
+                media.push(file);
+            }
+        } else {
+            var sources = elem.getElementsByTagName('source');
+            for (var j = 0; j < sources.length; j++) {
+                var source = sources[j];
+                if (regex.test(source.href)) {
+                    var file = {
+                        title: (elem.title) ? elem.title : null,
+                        url: source.href
+                    };
+                    media.push(file);
+                }
+            }
+        }
+    }
+};
 
 var getAudio = function() {
     var media = [];
     scrapeATags(media);
-    // scrapeAudioTags(media);
+    scrapeAudioTags(media);
     return media;
 };
 
